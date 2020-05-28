5303cd57b3b0f68380f098b0b40037e960677f9a
reshetnikd <dmitry.reshetnik@icloud.com>
2020-05-28 11:33:22 +0300

    Create first unit test using XCTest

---
+++ Shakespeare/PlayData.swift
@@ -0,0 +1,13 @@
+//
+//  PlayData.swift
+//  Shakespeare
+//
+//  Created by Dmitry Reshetnik on 28.05.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import Foundation
+
+class PlayData {
+    var allWords = [String]()
+}
