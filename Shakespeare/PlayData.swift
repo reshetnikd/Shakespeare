fc8b4ada15100fa361aef3cf2f8b00f85ffc2232
reshetnikd <dmitry.reshetnik@icloud.com>
2020-05-28 15:24:05 +0300

    Load data and split up words

--- Shakespeare/PlayData.swift
+++ Shakespeare/PlayData.swift
@@ -10,4 +10,13 @@ import Foundation
 
 class PlayData {
     var allWords = [String]()
+    
+    init() {
+        if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
+            if let plays = try? String(contentsOfFile: path) {
+                allWords = plays.components(separatedBy: CharacterSet.alphanumerics.inverted)
+                allWords = allWords.filter { $0 != "" }
+            }
+        }
+    }
 }
--- ShakespeareTests/ShakespeareTests.swift
+++ ShakespeareTests/ShakespeareTests.swift
@@ -21,7 +21,7 @@ class ShakespeareTests: XCTestCase {
     
     func testAllWordsLoaded() {
         let playData = PlayData()
-        XCTAssertEqual(playData.allWords.count, 0, "allWords must be 0")
+        XCTAssertEqual(playData.allWords.count, 384001, "allWords was not 384001")
     }
 
 }
