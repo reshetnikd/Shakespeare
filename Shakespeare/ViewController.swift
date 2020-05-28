bf80fcdfbe187695aabda827d7e04841a264b90d
reshetnikd <dmitry.reshetnik@icloud.com>
2020-05-28 10:35:06 +0300

    Initial Commit

---
+++ Shakespeare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shakespeare
+//
+//  Created by Dmitry Reshetnik on 28.05.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
