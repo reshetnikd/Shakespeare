f1ec7a70e92af2a88859f42d9f5e5da5e0adf4da
reshetnikd <dmitry.reshetnik@icloud.com>
2020-05-28 16:17:15 +0300

    Use measure() to optimise slow code

--- ShakespeareTests/ShakespeareTests.swift
+++ ShakespeareTests/ShakespeareTests.swift
@@ -26,9 +26,15 @@ class ShakespeareTests: XCTestCase {
     
     func testWordCountsAreCorrect() {
         let playData = PlayData()
-        XCTAssertEqual(playData.wordCounts["home"], 174, "Home does not appear 174 times")
-        XCTAssertEqual(playData.wordCounts["fun"], 4, "Fun does not appear 4 times")
-        XCTAssertEqual(playData.wordCounts["mortal"], 41, "Mortal does not appear 41 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "home"), 174, "Home does not appear 174 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "fun"), 4, "Fun does not appear 4 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "mortal"), 41, "Mortal does not appear 41 times")
+    }
+    
+    func testWordsLoadQuickly() {
+        measure {
+            _ = PlayData()
+        }
     }
 
 }
